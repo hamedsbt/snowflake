c7b163de57ebef3bb9e24fd6cd450c8633a6ca88
Shelikhoo <xiaokangwang@outlook.com>
2024-09-12 14:22:15 +0100

    Remove clientID from unused branch

--- client/lib/snowflake.go
+++ client/lib/snowflake.go
@@ -78,8 +78,6 @@ type Transport struct {
 	// EventDispatcher is the event bus for snowflake events.
 	// When an important event happens, it will be distributed here.
 	eventDispatcher event.SnowflakeEventDispatcher
-
-	clientID turbotunnel.ClientID
 }
 
 // ClientConfig defines how the SnowflakeClient will connect to the broker and Snowflake proxies.
@@ -169,7 +167,7 @@ func NewSnowflakeClient(config ClientConfig) (*Transport, error) {
 	clientID := turbotunnel.NewClientID()
 	transport := &Transport{
 		dialer:          NewWebRTCDialerWithNatPolicyAndEventsAndProxyAndClientID(broker, natPolicy, iceServers, max, eventsLogger, config.CommunicationProxy, clientID),
-		eventDispatcher: eventsLogger, clientID: clientID,
+		eventDispatcher: eventsLogger,
 	}
 
 	return transport, nil
@@ -204,7 +202,7 @@ func (t *Transport) Dial() (net.Conn, error) {
 
 	// Create a new smux session
 	log.Printf("---- SnowflakeConn: starting a new session ---")
-	pconn, sess, err := newSession(snowflakes, t.clientID)
+	pconn, sess, err := newSession(snowflakes)
 	if err != nil {
 		return nil, err
 	}
@@ -330,7 +328,7 @@ func parseIceServers(addresses []string) []webrtc.ICEServer {
 // newSession returns a new smux.Session and the net.PacketConn it is running
 // over. The net.PacketConn successively connects through Snowflake proxies
 // pulled from snowflakes.
-func newSession(snowflakes SnowflakeCollector, clientIDCandid turbotunnel.ClientID) (net.PacketConn, *smux.Session, error) {
+func newSession(snowflakes SnowflakeCollector) (net.PacketConn, *smux.Session, error) {
 	// We build a persistent KCP session on a sequence of ephemeral WebRTC
 	// connections. This dialContext tells RedialPacketConn how to get a new
 	// WebRTC connection when the previous one dies. Inside each WebRTC
